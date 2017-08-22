547bfd8be0a13ae89e8d60bd3db8afa623f25b93
Jason McFarland <jmcfarland@carbonblack.com>
2017-08-22 14:21:41 -0500

    bugfixes and inc version in integration_name

--- cbopensource/connectors/yara/bridge.py
+++ cbopensource/connectors/yara/bridge.py
@@ -73,7 +73,7 @@ class YaraProvider(BinaryAnalysisProvider):
 class YaraConnector(DetonationDaemon):
     @property
     def integration_name(self):
-        return 'Cb Yara Connector 1.2.10'
+        return 'Cb Yara Connector 1.2.11'
 
     @property
     def filter_spec(self):
