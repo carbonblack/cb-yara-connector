749c26298129b25f169d7230865c8f2c2b4e1484
Sharan Multani <smultani@carbonblack.com>
2018-03-16 09:50:41 -0400

    Version bump 1.3.4

--- cbopensource/connectors/yara/bridge.py
+++ cbopensource/connectors/yara/bridge.py
@@ -73,7 +73,7 @@ class YaraProvider(BinaryAnalysisProvider):
 class YaraConnector(DetonationDaemon):
     @property
     def integration_name(self):
-        return 'Cb Yara Connector 1.3.3'
+        return 'Cb Yara Connector 1.3.4'
 
     @property
     def filter_spec(self):
