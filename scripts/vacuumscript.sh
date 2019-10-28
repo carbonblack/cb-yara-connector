26323c262ebc8799907c46d4c34cadb39822b639
Zachary Estep <zestep@carbonblack.com>
2019-10-28 08:04:16 -0400

    script edit

--- scripts/vacuumscript.sh
+++ scripts/vacuumscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
-psql -p 5002 -d $PGDATABASE -U $PGUSERNAME -c "vacuum (full,analyze, verbose) binary_status;"
-psql -p 5002 -d $PGDATABASE -U $PGUSERNAME -c "vacuum (full,analyze, verbose) sensor_registrations;"
-psql -p 5002 -d $PGDATABASE -U $PGUSERNAME -c "vacuum (full,analyze, verbose) vt_write_events;"
+psql -p 5002 -d $PGDATABASE -U $PGUSER -c "vacuum (full,analyze, verbose) binary_status;"
+psql -p 5002 -d $PGDATABASE -U $PGUSER -c "vacuum (full,analyze, verbose) sensor_registrations;"
+psql -p 5002 -d $PGDATABASE -U $PGUSER -c "vacuum (full,analyze, verbose) vt_write_events;"
 # commented out; hangs!
-psql -p 5002 -d $PGDATABASE -U $PGUSERNAME -c "vacuum (full,analyze, verbose) storefiles;"
+psql -p 5002 -d $PGDATABASE -U $PGUSER -c "vacuum (full,analyze, verbose) storefiles;"
