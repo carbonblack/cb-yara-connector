48bdf42ec8ef98ea2de0577e8450e18aa791d620
Burton Choinski <bchoinski@carbonblack.com>
2019-10-31 09:11:13 -0400

    future prep work

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# Copyright © 2014-2019 VMware, Inc. All Rights Reserved.
+
+# noinspection PyUnusedName
+__author__ = "Carbon Black"
+# noinspection PyUnusedName
+__version__ = "2.1.0"
