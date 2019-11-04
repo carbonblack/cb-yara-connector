b906efd317a9e5f2371da4d27c2bf4069a73b050
Zachary Estep <zestep@carbonblack.com>
2019-11-04 15:42:55 -0500

    Remove extra app context"

--- src/config_handling.py
+++ src/config_handling.py
@@ -11,26 +11,17 @@ from celery import Celery
 import globals
 from exceptions import CbInvalidConfig
 
+from tasks import app
+
 logger = logging.getLogger(__name__)
 
 __all__ = ["ConfigurationInit", "app"]
-
-################################################################################
-# Celery app
-################################################################################
-
-app = Celery()
-# noinspection PyUnusedName
-app.conf.task_serializer = "pickle"
-# noinspection PyUnusedName
-app.conf.result_serializer = "pickle"
-# noinspection PyUnusedName
-app.conf.accept_content = {"pickle"}
 
 
 ################################################################################
 # Configuration reader/validator
 ################################################################################
+
 
 class ConfigurationInit(object):
     """
@@ -92,14 +83,18 @@ class ConfigurationInit(object):
         elif value == "remote":
             globals.g_remote = True
         else:
-            raise CbInvalidConfig(f"{self.source} has an invalid 'worker_type' ({value})")
+            raise CbInvalidConfig(
+                f"{self.source} has an invalid 'worker_type' ({value})"
+            )
 
-        globals.g_yara_rules_dir = self._as_path("yara_rules_dir", required=True, exists=True, is_dir=True)
+        globals.g_yara_rules_dir = self._as_path(
+            "yara_rules_dir", required=True, exists=True, is_dir=True
+        )
 
         # local/remote configuration data
         globals.g_cb_server_url = self._as_str("cb_server_url", required=True)
         globals.g_cb_server_token = self._as_str("cb_server_token", required=True)
-        
+
         value = self._as_str("broker_url", required=True)
         app.conf.update(broker_url=value, result_backend=value)
 
@@ -112,37 +107,69 @@ class ConfigurationInit(object):
         :raises CbInvalidConfig:
         :raises ValueError:
         """
-        globals.g_postgres_host = self._as_str("postgres_host", default=globals.g_postgres_host)
-        globals.g_postgres_username = self._as_str("postgres_username", default=globals.g_postgres_username)
+        globals.g_postgres_host = self._as_str(
+            "postgres_host", default=globals.g_postgres_host
+        )
+        globals.g_postgres_username = self._as_str(
+            "postgres_username", default=globals.g_postgres_username
+        )
         globals.g_postgres_password = self._as_str("postgres_password", required=True)
-        globals.g_postgres_db = self._as_str("postgres_db", default=globals.g_postgres_username)
-        globals.g_postgres_port = self._as_int("postgres_port", default=globals.g_postgres_port)
+        globals.g_postgres_db = self._as_str(
+            "postgres_db", default=globals.g_postgres_username
+        )
+        globals.g_postgres_port = self._as_int(
+            "postgres_port", default=globals.g_postgres_port
+        )
 
         value = self._as_int("niceness")
         if value:
             os.nice(value)
 
-        globals.g_max_hashes = self._as_int("concurrent_hashes", default=globals.g_max_hashes)
-        globals.g_disable_rescan = self._as_bool("disable_rescan", default=globals.g_disable_rescan)
-        globals.g_num_days_binaries = self._as_int("num_days_binaries", default=globals.g_num_days_binaries,
-                                                   min_value=1)
+        globals.g_max_hashes = self._as_int(
+            "concurrent_hashes", default=globals.g_max_hashes
+        )
+        globals.g_disable_rescan = self._as_bool(
+            "disable_rescan", default=globals.g_disable_rescan
+        )
+        globals.g_num_days_binaries = self._as_int(
+            "num_days_binaries", default=globals.g_num_days_binaries, min_value=1
+        )
 
-        globals.g_vacuum_interval = self._as_int("vacuum_interval", default=globals.g_vacuum_interval, min_value=0)
+        globals.g_vacuum_interval = self._as_int(
+            "vacuum_interval", default=globals.g_vacuum_interval, min_value=0
+        )
         if globals.g_vacuum_interval > 0:
-            globals.g_vacuum_script = self._as_path("vacuum_script", required=True, is_dir=False,
-                                                    default=globals.g_vacuum_script)
-            logger.warning(f"Vacuum Script '{globals.g_vacuum_script}' is enabled; " +
-                           "use this advanced feature at your own discretion!")
+            globals.g_vacuum_script = self._as_path(
+                "vacuum_script",
+                required=True,
+                is_dir=False,
+                default=globals.g_vacuum_script,
+            )
+            logger.warning(
+                f"Vacuum Script '{globals.g_vacuum_script}' is enabled; "
+                + "use this advanced feature at your own discretion!"
+            )
         else:
-            if self._as_path("vacuum_script", required=False, default=globals.g_vacuum_script):
-                logger.debug(f"{self.source} has 'vacuum_script' defined, but it is disabled")
+            if self._as_path(
+                "vacuum_script", required=False, default=globals.g_vacuum_script
+            ):
+                logger.debug(
+                    f"{self.source} has 'vacuum_script' defined, but it is disabled"
+                )
 
-        globals.g_feed_database_dir = self._as_path("feed_database_dir", required=True, is_dir=True,
-                                                    default=globals.g_feed_database_dir, create_if_needed=True)
+        globals.g_feed_database_dir = self._as_path(
+            "feed_database_dir",
+            required=True,
+            is_dir=True,
+            default=globals.g_feed_database_dir,
+            create_if_needed=True,
+        )
 
     # ----- Type Handlers
 
-    def _as_str(self, param: str, required: bool = False, default: str = None) -> Optional[str]:
+    def _as_str(
+        self, param: str, required: bool = False, default: str = None
+    ) -> Optional[str]:
         """
         Get a string parameter from the configuration.
 
@@ -155,19 +182,30 @@ class ConfigurationInit(object):
         try:
             value = self.the_config.get(param, None)
         except Exception as err:
-            raise CbInvalidConfig(f"{self.source} parameter '{param}' cannot be parsed: {err}")
+            raise CbInvalidConfig(
+                f"{self.source} parameter '{param}' cannot be parsed: {err}"
+            )
 
         if value is not None:
             value = value.strip()
         if (value is None or value == "") and default is not None:
             value = default
-            logger.warning(f"{self.source} has no defined '{param}'; using default of '{default}'")
+            logger.warning(
+                f"{self.source} has no defined '{param}'; using default of '{default}'"
+            )
         if required and (value is None or value == ""):
             raise CbInvalidConfig(f"{self.source} has no '{param}' definition")
         return value
 
-    def _as_path(self, param: str, required: bool = False, exists: bool = True, is_dir: bool = False,
-                 default: str = None, create_if_needed: bool = False) -> Optional[str]:
+    def _as_path(
+        self,
+        param: str,
+        required: bool = False,
+        exists: bool = True,
+        is_dir: bool = False,
+        default: str = None,
+        create_if_needed: bool = False,
+    ) -> Optional[str]:
         """
         Get an string parameter from the configuration and treat it as a path, performing normalization
         to produce an absolute path.  a "~/" at the beginning will be treated as the current user's home
@@ -193,19 +231,33 @@ class ConfigurationInit(object):
                     try:
                         os.makedirs(value)
                     except Exception as err:
-                        raise CbInvalidConfig(f"{self.source} unable to create '{value}' for '{param}': {err}")
+                        raise CbInvalidConfig(
+                            f"{self.source} unable to create '{value}' for '{param}': {err}"
+                        )
                 else:
-                    raise CbInvalidConfig(f"{self.source} specified path parameter '{param}' ({value}) does not exist")
+                    raise CbInvalidConfig(
+                        f"{self.source} specified path parameter '{param}' ({value}) does not exist"
+                    )
             if is_dir:
                 if not os.path.isdir(value):
-                    raise CbInvalidConfig(f"{self.source} specified path '{param}' ({value}) is not a directory")
+                    raise CbInvalidConfig(
+                        f"{self.source} specified path '{param}' ({value}) is not a directory"
+                    )
             else:
                 if os.path.isdir(value):
-                    raise CbInvalidConfig(f"{self.source} specified path '{param}' ({value}) is a directory")
+                    raise CbInvalidConfig(
+                        f"{self.source} specified path '{param}' ({value}) is a directory"
+                    )
 
         return value
 
-    def _as_int(self, param: str, required: bool = False, default: int = None, min_value: int = -1) -> Optional[int]:
+    def _as_int(
+        self,
+        param: str,
+        required: bool = False,
+        default: int = None,
+        min_value: int = -1,
+    ) -> Optional[int]:
         """
         Get an integer configuration parameter from the configuration.  A parameter that cannot be converted
         to an int will return a ValueError.
@@ -221,13 +273,19 @@ class ConfigurationInit(object):
         value = self._as_str(param, required)
         use_default = default if default is None else max(default, min_value)
         if (value is None or value == "") and use_default is not None:
-            logger.warning(f"{self.source} has no defined '{param}'; using default of '{use_default}'")
+            logger.warning(
+                f"{self.source} has no defined '{param}'; using default of '{use_default}'"
+            )
             return use_default
         else:
-            return None if (value is None or value == "") else max(int(value), min_value)
+            return (
+                None if (value is None or value == "") else max(int(value), min_value)
+            )
 
     # noinspection PySameParameterValue
-    def _as_bool(self, param: str, required: bool = False, default: bool = None) -> Optional[bool]:
+    def _as_bool(
+        self, param: str, required: bool = False, default: bool = None
+    ) -> Optional[bool]:
         """
         Get a boolean configuration parameter from the configuration.  A parameter not one of
         ["true", "yes", "false", "no"] will return a ValueError.
@@ -239,10 +297,20 @@ class ConfigurationInit(object):
         :raises ValueError:
         """
         value = self._as_str(param, required)
-        if value is not None and value.lower() not in ["true", "yes", "false", "no", ""]:
-            raise ValueError(f"{self.source} parameter '{param}' is not a valid boolean value")
+        if value is not None and value.lower() not in [
+            "true",
+            "yes",
+            "false",
+            "no",
+            "",
+        ]:
+            raise ValueError(
+                f"{self.source} parameter '{param}' is not a valid boolean value"
+            )
         if value is None and default is not None:
-            logger.warning(f"{self.source} has no defined '{param}'; using default of '{default}'")
+            logger.warning(
+                f"{self.source} has no defined '{param}'; using default of '{default}'"
+            )
             return default
         else:
             return value if value is None else value.lower() in ["true", "yes"]
