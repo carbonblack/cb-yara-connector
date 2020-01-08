2f08560acfe5c5336255f868e97ca3ea6f29c495
Zachary Estep <zestep@carbonblack.com>
2020-01-08 15:32:05 -0500

    updates

--- docker-build-rpm.sh
+++ docker-build-rpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker rmi yaraconnectorrpmbuild --force
 docker rm yaraconnectorrpmbuild --force
-docker build -t yaraconnectorrpmbuild .	
+docker build -t yaraconnectorrpmbuild . --no-cache	
 docker run  -d --name yaraconnectorrpmbuild -it yaraconnectorrpmbuild tail -f /dev/null
 docker cp yaraconnectorrpmbuild:/home/cb/rpmbuild/RPMS .
 docker stop yaraconnectorrpmbuild 
