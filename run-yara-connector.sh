56019b9c23101da795052f1f4fb2af7564412955
Zachary Estep <zestep@carbonblack.com>
2022-02-01 14:09:45 -0500

    CB-37038: Production Docker container build for yara-connector

--- run-yara-connector.sh
+++ run-yara-connector.sh
@@ -2,16 +2,21 @@
 LABEL=edryaraconnector
 IMAGE=yaraconnector/centos7:latest
 EDR_MODULE_STORE=/var/cb/data/modulestore
-CONFIG_SOURCE_DIR=/etc/cb/integrations/cb-yara-connector
-LOG_DIR=/var/cb/integrations/cb-yara-connector
+CONFIG_DIR=/etc/cb/integrations/cb-yara-connector
+LOG_DIR=/var/log/cb/integrations/cb-yara-connector
 DATA_DIR=/var/cb/data/cb-yara-connector
-MOUNT_POINTS="--mount type=bind,source=$CONFIG_SOURCE_DIR,target=/etc/cb/integrations/cb-yara-connector --mount type=bind,source=$DATA_DIR,target=$DATA_DIR --mount type=bind,source=$LOG_DIR,target=$LOG_DIR --mount type=bind,source=$EDR_MODULE_STORE,target=$EDR_MODULE_STORE" 
+MOUNT_POINTS="--mount type=bind,source=$CONFIG_DIR,target=/etc/cb/integrations/cb-yara-connector --mount type=bind,source=$DATA_DIR,target=$DATA_DIR --mount type=bind,source=$LOG_DIR,target=$LOG_DIR --mount type=bind,source=$EDR_MODULE_STORE,target=$EDR_MODULE_STORE"
 SERVICE_START='systemctl start cb-yara-connector'
 SERVICE_STOP='systemctl stop cb-yara-connector'
 SERVICE_RESTART='systemctl restart cb-yara-connector'
-STARTUP_COMMAND="docker run --rm $MOUNT_POINTS --name $LABEL $IMAGE $SERVICE_START"
-echo $STARTUP_COMMAND
-SHUTDOWN_COMMAND="docker stop $LABEL" 
+CONTAINER_RUNNING=$(docker inspect --format="{{.State.Running}}" $LABEL 2> /dev/null)
+CONTAINER_STOPPED=$(docker inspect --format="{{.State.Stopped}}" $LABEL 2> /dev/null)
+if [ "$RUNNING" == "false" ]; then
+  STARTUP_COMMAND="docker run -d --restart unless-stopped	--rm $MOUNT_POINTS --name $LABEL $IMAGE $SERVICE_START"
+else
+  STARTUP_COMMAND="echo EDR Yara Connector container already running"
+fi
+SHUTDOWN_COMMAND="docker stop $LABEL"
 START_COMMAND="docker exec $LABEL $SERVICE_START"
 RESTART_COMMAND="docker exec $LABEL $SERVICE_RESTART"
 STOP_COMMAND="docker exec $LABEL $SERVICE_STOP"
@@ -46,6 +51,9 @@ if [[ "${1^^}" =~ ^(STARTUP|SHUTDOWN|START|STOP|RESTART)$ ]]; then
   case "${1^^}" in
     STARTUP) $STARTUP_COMMAND ;;
     SHUTDOWN) $SHUTDOWN_COMMAND ;;
+    START)  $START_COMMAND ;;
+    STOP) $STOP_COMMAND ;;
+    RESTART) $RESTART_COMMAND ;;
   esac
 else
   echo "run: invalid command '${1}'"; print_help
